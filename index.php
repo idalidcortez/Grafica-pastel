446f4aaa96038409ea2e9697eb520d383652e905
Kei Idalid <idalidcortez@gmail.com>
2023-04-19 10:42:49 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <h1> Gráfica de Pastel: Idalid </h1> 
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Pantalla LED',     6],
+          ['Mouse USB',      16],
+          ['Disco duro 1TB',  14],
+          ['Teclado USD', 8],
+          ['Monitor LG 21',    6],
+          ['Lector 3nStar', 20]
+        ]);
+
+        var options = {
+          title: 'Productos'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
